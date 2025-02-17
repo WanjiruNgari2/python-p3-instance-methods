f2268d22f10240818ebb9129730e8094d3e20702
Damaris Ngari <wanjirungari2@gmail.com>
2025-02-17 11:43:44 +0300

    dog.py

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,18 @@
 class Dog:
     # Class body goes here
 
+    def __init__(self, name="dog"):
+        self.name = name
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print(f"The {self.name} is sitting.")
+
+
+#finish by calling
+dog1 = Dog("Dog")
+dog1.bark()
+
     #Instance method definition
-    pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,21 @@
 
 class Person:
     # Class body goes here
+    def __init__(self, name = "Person"):
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+#call 
+p1 = Person("Guido")
+p1.talk()
+p1.walk()
+
+
 
     #Instance method definition
-    pass
+ 
